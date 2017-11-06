5dd79db50c4d289cff029194ad83fef6ba298423
OtakuEmperor <g3odshackoe@gmail.com>
2017-11-06 13:57:45 +0800

    complete garage algorithm

--- array/garage.py
+++ array/garage.py
@@ -3,13 +3,21 @@ def garage(init, final):
     while init != final:
         zero = init.index(0)
         if zero != final.index(0):
-            finalZero = final.index(0)
-            index[zero], index[finalZero] = index
+            carMove = final[zero]
+            carTo = init.index(carMove)
+            init[zero], init[carTo] = init[carTo], init[zero]
+        else :
+            for i in range(len(init)):
+                if init[i] != final[i]:
+                    init[zero], init[i] = init[i], init[zero]
+                    break
+        step += 1
+    return step
 
 
 
 
 
 init = [1,2,3,0,4]
-final = [0,3,1,2,4]
+final = [0,3,2,1,4]
 print(garage(init, final))
