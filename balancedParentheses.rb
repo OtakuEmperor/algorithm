b594ec880f52735a13a4657dc094e5cc8f9fd1c3
曾文祐 <g3odshackoe@gmail.com>
2017-11-16 23:47:35 +0800

    Create balancedParentheses.rb

---
+++ balancedParentheses.rb
@@ -0,0 +1,14 @@
+def balanced_parentheses?(str)
+  stack = []
+  str.chars.each do |token|
+    case token
+      when '('
+        stack.push '('
+      when ')'
+        if stack.empty? || stack.pop != '('
+          return false
+        end
+    end
+  end
+  stack.empty?
+end
